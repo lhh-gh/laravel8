f9147838195a86585373fb8b6f669e9c87f5025f
liuhuihui <627146930@qq.com>
2025-04-13 21:57:31 +0800

    自定义辅助函数

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: SillyCat
+ * Date: 2025-04-12
+ * Time: 20:31
+ */
+
+namespace App\Http\Controllers;
+
+class TestController
+{
+    public function index()
+    {
+        return 'test';
+    }
+                        public function index2()
+                        {
+                            return 'test2';
+                        }
+
+}
---
+++ bootstrap/helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: SillyCat
+ * Date: 2025-04-13
+ * Time: 21:45
+ */
+function test_helper() {
+    return 'OK';
+}
